4e0e91b4e53a963e051a15f07590464d5b40527a
lixiaoyutian <1259672955@qq.com>
2022-02-23 20:44:34 +0800

    实现最大公约数

---
+++ 最大公约数/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+WriteLine(GCD(25, 10));
+
+int GCD(int a, int b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    if ((a & 1) == 0 && (b & 1) == 0)
+    {
+        return GCD(a >> 1, b >> 1);
+    }
+    else if ((a & 1) == 0 && (b & 1) != 0)
+    {
+        return GCD(a >> 1, b);
+    }
+    else if ((a & 1) == 0 && (b & 1) != 0)
+    {
+        return GCD(a, b >> 1);
+    }
+    else
+    {
+        int big = a > b ? a : b;
+        int small = a < b ? a : b;
+        return GCD(small, big - small);
+    }
+}
