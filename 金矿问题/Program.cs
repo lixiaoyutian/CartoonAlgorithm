16a8a3f8071d220e7eb91b8bd1234681354e5ef1
lixiaoyutian <1259672955@qq.com>
2022-03-09 21:36:35 +0800

    实现金矿问题

---
+++ 金矿问题/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+int w = 10;
+int[] p = new int[] { 5, 5, 3, 4, 3 };
+int[] g = new int[] { 400, 500, 200, 300, 350 };
+WriteLine(getBestGold(w, p, g));
+
+
+int getBestGold(int w, int[] p, int[] g)
+{
+    int[] result = new int[w + 1];
+    for (int i = 1; i <= g.Length; i++)
+    {
+        for (int j = w; j >= 1; j--)
+        {
+            if (j >= p[i - 1])
+            {
+                result[j] = Math.Max(result[j], result[j - p[i - 1]] + g[i - 1]);
+            }
+        }
+    }
+    return result[w];
+}
