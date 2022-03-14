21b7eaa204fd705ad70dbf39ab16f779a26ad3b0
lixiaoyutian <1259672955@qq.com>
2022-03-14 20:38:27 +0800

    完成寻找缺失的整数

---
+++ 寻找缺失的整数/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+int[] result = findLostNum(new int[] { 4, 1, 2, 2, 5, 1, 4, 3 });
+foreach (var item in result)
+{
+    Write(item + " ");
+}
+WriteLine();
+
+int[] findLostNum(int[] array)
+{
+    int[] result = new int[2];
+    int xorResult = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        xorResult ^= array[i];
+    }
+    if (xorResult == 0)
+    {
+        return null;
+    }
+    int separator = 1;
+    while (0 == (xorResult & separator))
+    {
+        separator <<= 1;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((separator & array[i]) == 0)
+        {
+            result[0] ^= array[i];
+        }
+        else
+        {
+            result[1] ^= array[i];
+        }
+    }
+    return result;
+}
