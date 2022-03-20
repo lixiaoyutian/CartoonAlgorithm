65465d0347ec46a28f860cd7a2e5ccb677e33014
lixiaoyutian <1259672955@qq.com>
2022-03-20 17:27:56 +0800

    完成红包算法

---
+++ 红包算法/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+List<int> list = divideRedPackage(1000, 10);
+int temp = 0;
+foreach (var item in list)
+{
+    WriteLine(item);
+    temp += item;
+}
+WriteLine(temp);
+List<int> divideRedPackage(int totalAmout, int totalPeopleNum)
+{
+    List<int> amoutList = new List<int>();
+    int tempAmout = totalAmout;
+    int tempPeople = totalPeopleNum;
+    Random random = new Random();
+    for (int i = 1; i < totalPeopleNum; i++)
+    {
+        int amout = random.Next(tempAmout / tempPeople * 2 - 1) + 1;
+        tempPeople--;
+        tempAmout -= amout;
+        amoutList.Add(amout);
+    }
+    amoutList.Add(tempAmout);
+    return amoutList;
+}
